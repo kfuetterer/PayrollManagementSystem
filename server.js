7fea011c1f90c9040d91c549cbc35e5647d7d74e
Katharina Fuetterer <Katharina Fuetterer>
2017-07-14 14:47:12 -0400

    heroku

--- server.js
+++ server.js
@@ -10,20 +10,6 @@ var db = require("./models");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('./webpack.dev.config.js')
-  const compiler = webpack(config)
-
-  app.use(webpackHotMiddleware(compiler))
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }))
-}
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
